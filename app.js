6fbdd8138b502b634e39d6f02ca3a8bcbb419be8
l-martinetti <luca.martinetti@outlook.it>
2024-12-18 09:52:55 +0100

    feat: add static assets and bacheca post counter
    ex. complete

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const port = 3000;
 
 const posts = require("./posts")
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) =>{
     res.send('Server del mio blog');
+})
+
+app.get('/bacheca', (req, res) => {
+    const post = {
+        postsCounter: posts.length,
+        postList: posts
+    };
+    res.json(post) 
 })
 
 app.listen(port, err => {
     
 })
-
 
--- posts.js
+++ posts.js
@@ -2,31 +2,31 @@ const posts = [
     {
         title: "Un ciambellone",
         contet: "ciambellone",
-        image: "./images/ciambellone.jpeg",
+        image: "/images/ciambellone.jpeg",
         tags: ["dolci", "festa", "cucina"]
     },
     {
         title: "il cracker alla barbabietola",
         contet: "creacker alla barbabietola",
-        image: "./images/cracker_barbabietola.jpeg",
+        image: "/images/cracker_barbabietola.jpeg",
         tags: ["cracker", "barbabietola", "sfiziosita"]
     },
     {
         title: "Il pane fritto dolce",
         contet: "pane fritto dolce",
-        image: "./images/pane_fritto_dolce.jpeg",
+        image: "/images/pane_fritto_dolce.jpeg",
         tags: ["pane", "dolce", "fritto"]
     },
     {
         title: "La pasta alla barbabietola",
         contet: "pasta barbabietola",
-        image: "./images/pasta_barbabietola.jpeg",
+        image: "/images/pasta_barbabietola.jpeg",
         tags: ["pasta", "barbabietola"]
     },
     {
         title: "La torta paesana",
         contet: "torta paesana",
-        image: "./images/torta_paesana.jpeg",
+        image: "/images/torta_paesana.jpeg",
         tags: ["torta", "paesana", "dolce"]
     }
 ];
